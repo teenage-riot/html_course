af6565698cad5cb6c52eb43c5f234de8d306007c
Глеб Ивакин <kublahanych@gmail.com>
2024-05-23 18:13:23 +0300

    customers section well done

--- auto/scripts/main.js
+++ auto/scripts/main.js
@@ -10,7 +10,7 @@ $(function () {
     ratedFill: "#ffb648",
     starSvg: '<svg width="27" height="27" viewBox="0 0 27 27" xmlns="http://www.w3.org/2000/svg">' +
       '<path d="M11.36 0.82L9.07 5.03C8.64 5.82 7.87 6.37 6.99 6.54L2.28 7.42C-0.05 7.86 -0.98 10.7 0.66 12.43L3.94 15.91C4.56 16.56 4.85 17.46 4.74 18.36L4.12 23.1C3.82 25.46 6.24 27.22 8.38 26.2L12.71 24.15C13.52 23.76 14.47 23.76 15.28 24.15L19.61 26.2C21.75 27.22 24.17 25.46 23.87 23.1L23.25 18.36C23.14 17.46 23.43 16.56 24.05 15.91L27.33 12.43C28.97 10.7 28.04 7.86 25.71 7.42L21 6.54C20.12 6.37 19.35 5.82 18.92 5.03L16.63 0.82C15.49 -1.26 12.5 -1.26 11.36 0.82Z"/>' +
-      '</svg>'
+      '</svg>',
   });
 
 
@@ -23,11 +23,21 @@ $(function () {
 
 
   $('.customers__slider').slick({
+    appendDots: $('.customer__slider-dots'),
     arrows:false,
+    dots: true,
     draggable: false,
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 2,
+  })
+
+  $('.customers__arrow-prev').on('click', function() {
+    $('.customers__slider').slick('slickPrev');
+  })
+
+  $('.customers__arrow-next').on('click', function() {
+    $('.customers__slider').slick('slickNext');
   })
 
 });
